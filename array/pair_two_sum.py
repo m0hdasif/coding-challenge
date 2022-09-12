e09b27fa670d2375a259269650114503e129048b
M Asif <asifmansoori@outlook.com>
2022-09-13 00:35:26 +0530

    get pairs of values for given target sum

--- array/pair_two_sum.py
+++ array/pair_two_sum.py
@@ -11,6 +11,26 @@ def pair_sum(nums: list[int], target: int) -> list[int]:
     return pairs
 
 
+# T(n) = O(nlogn) for sorting, S(n)=O(1)
+# method using 2 pointers to lists
+def sorted_pair_sum_v2(nums: list[int], target: int) -> list[int]:
+    nums.sort()
+    start_index = 0
+    end_index = max(0, len(nums) - 1)
+    pairs = []
+    while start_index < end_index:
+        sum_val = nums[start_index] + nums[end_index]
+        if sum_val == target:
+            pairs.append((nums[start_index], nums[end_index]))
+            start_index += 1
+        elif sum_val > target:
+            end_index -= 1
+        else:
+            start_index += 1
+
+    return pairs
+
+
 def unique_pair_sum(nums: list[int], target: int) -> list[int]:
     return list(set(pair_sum(nums, target)))
 
@@ -23,6 +43,7 @@ def sorted_unique_pair_sum(nums: list[int], target: int) -> list[int]:
 
 
 # T(n)= O(nlogn) for sorting
+# S(n) = O(n) for worst case
 def sorted_unique_pair_sum_v2(nums: list[int], target: int) -> list[int]:
     complement_value_map = {}
     pairs = []
@@ -40,5 +61,6 @@ if __name__ == "__main__":
     arr = [1, 2, 3, 6, 4, 6, 5, 8, 9]
     target = 9
     print(f"{pair_sum(arr,target)=}")  # 4 elements
+    print(f"{sorted_pair_sum_v2(arr,target)=}")
     print(f"{sorted_unique_pair_sum(arr,target)=}")
     print(f"{sorted_unique_pair_sum_v2(arr,target)=}")
