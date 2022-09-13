55cbfad900f37868d34c8d9e79e6459e6b2cab1e
M Asif <asifmansoori@outlook.com>
2022-09-13 13:27:24 +0530

    add three sum pairs alternatives

--- array/three_sum.py
+++ array/three_sum.py
@@ -1,12 +1,16 @@
 # constraint -> all 3 indexes should be different
+from pair_two_sum import sorted_pair_sum_v2
+
 # Sol1: brute force- check elements sum is equal to target from i->0-n, j ->i+1-n, k->j+1-n
 # T(n) = O(n^3)
-
-from pair_two_sum import sorted_pair_sum_v2
+# S(n) = O(1)
 
 
 # Sol2: use 2 sum to use the three_sum
 # T(n)-> O(n)*O(nlogn) => O(n^2logn)
+# S(n) = O(1)
+
+
 def three_sum(arr: list[int], target) -> list[int]:
     pairs = []
     for index, elem in enumerate(arr):
@@ -15,6 +19,13 @@ def three_sum(arr: list[int], target) -> list[int]:
     return pairs
 
 
+# Sol3: use the above solution to sort first then iterate over the loop and find the two sum pairs
+# T(n): O(n^2) # [O(nlogn) +O(n^2)]
+# S(n): O(1)
+
+# Sol4 : using 3 pointers -> basic logic is similar to above
+# T(n): O(n^2) # [O(nlogn) +O(n^2)]
+# S(n): O(1)
 if __name__ == "__main__":
     arr = [-1, 0, 1, 2, -1, -4, 2, 5]
     target = 1
