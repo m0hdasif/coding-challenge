eb60dc6f9da493e57d08e90cfcd1227648e5d3d1
M Asif <asifmansoori@outlook.com>
2022-09-14 16:56:56 +0530

    find total occurrence of a given element in array

--- array/first_and_last_occurrence.py
+++ array/first_and_last_occurrence.py
@@ -1,7 +1,7 @@
 # Using binary search to find the first and last occurrence of the given match
 
-
-def find_first_occurrence(arr: list[int], element: int) -> list[int]:
+# T(n)= O(logn)
+def find_first_occurrence(arr: list[int], element: int) -> int:
     start = 0
     end = len(arr) - 1
     first_index = -1
@@ -17,7 +17,8 @@ def find_first_occurrence(arr: list[int], element: int) -> list[int]:
     return first_index
 
 
-def find_last_occurrence(arr: list[int], element: int) -> list[int]:
+# T(n)= O(logn)
+def find_last_occurrence(arr: list[int], element: int) -> int:
     start = 0
     end = len(arr) - 1
     last_index = -1
@@ -33,8 +34,15 @@ def find_last_occurrence(arr: list[int], element: int) -> list[int]:
     return last_index
 
 
+def total_occurrences(arr: list[int], element: int) -> int:
+    first_index = find_first_occurrence(arr, element)
+    last_index = find_last_occurrence(arr, element)
+    return last_index - first_index + 1 if first_index > 0 else 0
+
+
 if __name__ == "__main__":
     arr = [1, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 6, 7, 8]
     el = 4
     print(f"{find_first_occurrence(arr, el)=}")
     print(f"{find_last_occurrence(arr, el)=}")
+    print(f"{total_occurrences(arr, el)=}")
