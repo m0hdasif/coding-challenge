c69e13ddf7c99837577c5b704eda5e0f27163463
M Asif <asifmansoori@outlook.com>
2022-09-20 16:06:03 +0530

    add insertion sort algorithm

---
+++ array/sorting/insertion_sort.py
@@ -0,0 +1,16 @@
+# card like sorting, sort small block then keep sorting new element in each turn
+# T(n) = O(n^2) for worst and average case and O(n) for best case
+# Good for smaller arrays
+def insertion_sort(arr: list[int]) -> list[int]:
+    for index, _ in enumerate(arr):
+        i = index
+        while i > 0 and arr[i] < arr[i - 1]:
+            arr[i - 1], arr[i] = arr[i], arr[i - 1]
+            i -= 1
+
+    return arr
+
+
+if __name__ == "__main__":
+    arr = [5, 3, 1, 4, 1, 1, 2, 4, 6, 10, 8]
+    print(f"{insertion_sort(arr)}")
