185fe8479719631c6cfdbdead722ea796a2e0ae8
M Asif <asifmansoori@outlook.com>
2022-10-15 10:24:19 +0530

    unique element in sorted double occurrence array

--- array/unique_element_in_double_occurrence_array.py
+++ array/unique_element_in_double_occurrence_array.py
@@ -6,5 +6,19 @@ def get_unique_element(arr: list[int]) -> int:  # T(n) => O(n), S(n)=O(1)
     return ans
 
 
+def get_unique_element_from_sorted_array(
+    arr: list[int], start=0, end=None
+) -> int:  # T(n) => O(logn), S(n)=O(1)
+    if end is None:
+        end = len(arr) - 1
+    if start == end:
+        return arr[start]
+    mid = (start + end) // 2
+    if arr[mid] == arr[mid + 1]:
+        return get_unique_element_from_sorted_array(arr, start, mid - 1)
+    return get_unique_element_from_sorted_array(arr, mid + 1, end)
+
+
 if __name__ == "__main__":
-    print(get_unique_element([1, 3, 4, 5, 4, 5, 1]))  #
+    print(get_unique_element([1, 3, 4, 5, 4, 5, 1]))
+    print(get_unique_element_from_sorted_array([1, 1, 4, 4, 5, 5, 6]))
